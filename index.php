084fe6df22ea7a208c11e93e4165968f0a76db8d
mas0110 <masoudmohammed13@gmail.com>
2024-02-26 17:22:50 +0100

    form con input parola e testo

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>intro php</title>
+</head>
+<body>
+    <form action="text.php" method="GET" >
+        <label for="nome">parola da censurare:</label><br>
+        <input type="text" name="nome" id="nome" $parola><br>
+        <label for="testo"> inserisci la parola in un testo</label><br>
+        <textarea name="testo" id="testo" cols="30" rows="10"></textarea>
+        <button type="submit">invia</button>
+    </form>
+</body>
+</html>
